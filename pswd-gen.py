43459b87d2d3ad3a161a40e5511e4e14ff497482
ares1345 <ares1345istaken@gmail.com>
2024-04-16 08:56:00 +0200

    few changes

--- pswd-gen.py
+++ pswd-gen.py
@@ -1,14 +1,20 @@
 from random import *
 
+alph_upp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alph_low = 'abcdefghijklmnopqrstuvwxyz'
+spec_char = '!@#$%^&*()_+'
+
+
 def psswd_gen(num, diff):
-    alph_upp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    alph_low = 'abcdefghijklmnopqrstuvwxyz'
     psswd = ''
     cpt_l = 0
     cpt_n = 0
+    cpt_s = 0
     if diff == 'e':
         for i in range(num):
             psswd += str(randint(0, 100))
+
+
     elif diff == 'm':
         for i in range(num):
             r = randint(0, 1)
@@ -28,6 +34,16 @@ def psswd_gen(num, diff):
                 psswd += str(randint(0, 100))
                 cpt_n += 1
     
+    else:
+        for i in range(num):
+            r = randint(0, 2)
+            if cpt_l >= num//2:
+                r = 1
+            elif cpt_n >= num//2:
+                r = 0
+            elif cpt_s >= num//2:
+                r = randint(0, 1) #this is horrible
+            
 
     return psswd
 
