719dfd4d3d9515d33d42ab38743494adc5730155
ares1345 <ares1345istaken@gmail.com>
2024-04-16 09:21:46 +0200

    finished

--- pswd-gen.py
+++ pswd-gen.py
@@ -12,7 +12,7 @@ def psswd_gen(num, diff):
     cpt_s = 0
     if diff == 'e':
         for i in range(num):
-            psswd += str(randint(0, 100))
+            psswd += str(randint(0, 9))
 
 
     elif diff == 'm':
@@ -31,7 +31,7 @@ def psswd_gen(num, diff):
                     psswd += alph_low[randint(0, 25)]
                 cpt_l += 1
             else:
-                psswd += str(randint(0, 100))
+                psswd += str(randint(0, 9))
                 cpt_n += 1
     
     else:
@@ -44,8 +44,22 @@ def psswd_gen(num, diff):
             elif cpt_s >= num//2:
                 r = randint(0, 1) #this is horrible
             
+            if r == 0:
+                r = randint(0, 1)
+                if r == 0:
+                    psswd += alph_upp[randint(0, 25)]
+                else:
+                    psswd += alph_low[randint(0, 25)]
+                cpt_l += 1
+            elif r == 1:
+                
+                psswd += str(randint(0, 9))
+            else:
+                psswd += spec_char[randint(0, 11)]
 
     return psswd
 
-a = int(input(""))
-print(psswd_gen(a, 'm'))
+a = int(input("Number of characters in password?\n"))
+diff = str(input("Difficulty level? (e, m or leave blank for hard difficulty.)\n"))
+while diff not in ["e", "m", ""]: diff = str(input("Error, character not recognised, please re-enter character. \n")) 
+print(psswd_gen(a, diff))
